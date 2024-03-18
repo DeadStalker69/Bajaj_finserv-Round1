b49acb44c1f759e31005b12dc2e5b8991b835ab7
PrabhatGoyal <106471305+DeadStalker69@users.noreply.github.com>
2024-03-18 15:02:52 +0530

    Update file2.java

--- file2.java
+++ file2.java
@@ -1,16 +1,39 @@
-HttpClient httpClient = HttpClientBuilder.create().build();
-try {
-    HttpPost request = new HttpPost("https://customer-analytics-34146.my.salesforce-sites.com/services/apexrest/buyStocks");
-    StringEntity params = new StringEntity("details={
-"company": "Bajaj Finserv",
-"currentPrice": '1571.5,
-"accountNumber": BFHL0018634,
-"githubRepoLink" : "https://github.com/DeadStalker69/Bajaj_finserv-Round1.git"
-}");
-    request.addHeader("content-type": "application/json", "bfhl-auth" : "2110994829");
-    request.setEntity(params);
-    HttpResponse response = httpClient.execute(request);
-} catch (Exception ex) {
-} finally {
-    // @Deprecated httpClient.getConnectionManager().shutdown(); 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String url = "https://customer-analytics-34146.my.salesforce-sites.com/services/apexrest/buyStocks";
+        String json = "{\"company\":\"Bajaj Finserv Ltd\",\"currentPrice\":\1577,\"accountNumber\":\"BFHL0018634\", \"githubRepoLink\":\"https://github.com/DeadStalker69/Bajaj_finserv-Round1.git\"}";
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setRequestProperty("bfhl-auth", "2110994829");
+        con.setDoOutput(true);
+
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(json);
+        wr.flush();
+        wr.close();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("Response Code: " + responseCode);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        System.out.println(response.toString());
+    }
 }
